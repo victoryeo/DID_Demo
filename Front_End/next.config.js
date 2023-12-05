ee29802c017ee0b91017003b7631c7249084e520
yeosiowvic <victoryeo1974@gmail.com>
2023-12-05 16:22:23 +0800

    adding the outputFileTracingRoot property

--- Front_End/next.config.js
+++ Front_End/next.config.js
@@ -4,6 +4,9 @@ module.exports = {
       NX_VERIFICATION_SERVER_LOCAL_HOST_URL: process.env.NX_VERIFICATION_SERVER_LOCAL_HOST_URL,
       NX_WALLET_CONNECT_ID: process.env.NX_WALLET_CONNECT_ID,
       NX_VERIFICATION_SERVER_PUBLIC_URL: process.env.NX_VERIFICATION_SERVER_PUBLIC_URL
-    }
+    },
+    experimental: {
+      outputFileTracingRoot: path.join(__dirname, "../../"),
+    },
   }
   
