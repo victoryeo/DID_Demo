843065147f858f59b8eb61b39992eef233caec56
yeosiowvic <victoryeo1974@gmail.com>
2023-12-05 16:46:33 +0800

    remove outputFileTracingRoot

--- Front_End/next.config.js
+++ Front_End/next.config.js
@@ -5,8 +5,5 @@ module.exports = {
       NX_WALLET_CONNECT_ID: process.env.NX_WALLET_CONNECT_ID,
       NX_VERIFICATION_SERVER_PUBLIC_URL: process.env.NX_VERIFICATION_SERVER_PUBLIC_URL
     },
-    experimental: {
-      outputFileTracingRoot: path.join(__dirname, "../../"),
-    },
   }
   
